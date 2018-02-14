cfd750c80a3a462b8046234bd8e43a56e9e19173
unknown <brandonqr1@gmail.com>
2018-02-14 12:19:01 +0100

    agregada propiedad para nombreEmpresa

---
+++ PracticaPersona/Vendedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPersona
+{
+    class Vendedor
+    {
+        private string nombre;
+        private int ventas;
+        private string nombreEmpresa;
+
+        public Vendedor(string nombre, int ventas, string nombreEmpresa)
+        {
+            this.nombre = nombre;
+            this.ventas = ventas;
+            this.nombreEmpresa = nombreEmpresa;
+        }
+    }
+}
